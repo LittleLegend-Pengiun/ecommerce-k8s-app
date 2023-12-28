5425fbcba941362c401caf3f689f6f2254980437
Hoang Anh <hoanganhleboy@gmail.com>
2023-12-28 16:45:00 +0700

    update index.js

---
+++ apps/backend/catalog-ms-1/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import * as prometheusExporter from '@tailorbrands/node-exporter-prometheus';
+
+const app = express();
+
+const options = {
+    appName: "catalog-api",
+    collectDefaultMetrics: true,
+    ignoredRoutes: ['/metrics']
+};
+const promExporter = prometheusExporter(options);
+app.use(promExporter.middleware);
+app.get('/metrics', promExporter.metrics);
+app.get("/", (req, res) => { 
+    res.send("OK!");
+});
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(
+        `Catalog-node listening at 127.0.0.1:${port}`,
+    );
+});
+
