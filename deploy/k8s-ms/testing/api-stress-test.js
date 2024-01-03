0f7c03510d14719fc37b5bcaef2a10e4699d1bc3
LittleLegend-Pengiun <65987402+LittleLegend-Pengiun@users.noreply.github.com>
2024-01-03 21:58:34 +0700

    [US100401] Apply prometheus and keda to a microservice (#5)

---
+++ deploy/k8s-ms/testing/api-stress-test.js
@@ -0,0 +1,31 @@
+import { check, sleep } from "k6";
+import http from "k6/http";
+
+// export let options = {
+//   duration: "3m",
+//   vus: 200,
+//   thresholds: {
+//     http_req_duration: ["p(95)<700"]
+//   }
+// };
+
+export let options = {
+  stages: [
+    { duration: "1m", target: 50 },
+    // { duration: "1m", target: 150 },
+    // { duration: "1m", target: 300 },
+    // { duration: '2m', target: 500 },
+    // { duration: '2m', target: 800 },
+    { duration: '2m', target: 1200 },
+    { duration: "2m", target: 10000000 },
+    { duration: "2m", target: 10 },
+  ],
+};
+
+export default function () {
+  let r = http.get(`http://127.0.0.1:50833`);
+  check(r, {
+    "status is 200": (r) => r.status === 200,
+  });
+  sleep(3);
+}
