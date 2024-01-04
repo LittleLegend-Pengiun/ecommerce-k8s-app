ced461b6864e3625c16af33a54acb004c6895c5b
Hoàng Văn Hiếu <hieu.hoang2901@hcmut.edu.vn>
2024-01-04 16:01:49 +0700

    add pagination feature for display all products

--- apps/frontend/catalog-fe/src/pages/user/HomePage/index.js
+++ apps/frontend/catalog-fe/src/pages/user/HomePage/index.js
@@ -78,10 +78,11 @@ import { memo } from "react";
 import { AiOutlineEye, AiOutlineShoppingCart } from "react-icons/ai";
 // import Carousel from "react-multi-carousel";
 import { Link } from "react-router-dom";
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+// import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import { formatPrice } from "utils/formater";
 import "./style.scss";
-//console.log(process.env.REACT_APP_ENV);
+import React, { useState} from "react";
+import ReactPaginate from 'react-paginate';
 const HomePage = () => {
   const featProducts = {
     all: {
@@ -800,61 +801,39 @@ const HomePage = () => {
       ]
     }
   };
-
-  const renderFeaturedProducts = (data) => {
-    const tabList = [];
-    const tabPanels = [];
-
-    Object.keys(data).forEach((key, index) => {
-      tabList.push(<Tab key={index}>{data[key].title}</Tab>);
-
-      const tabPanel = [];
-      data[key].products.forEach((item, index) => {
-        tabPanel.push(
-          <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12" key={index}>
-            <div className="featured__item pl-pr-10">
-              <div
-                className="featured__item__pic set-bg"
-                style={{
-                  backgroundImage: `url(${item.img})`,
-                }}
-              >
-                <ul className="featured__item__pic__hover">
-                  <li>
-                    <AiOutlineEye />
-                  </li>
-                  <li>
-                    <AiOutlineShoppingCart />
-                  </li>
-                </ul>
-              </div>
-              <div className="featured__item__text">
-                <h6>
-                  <Link to="">{item.name}</Link>
-                </h6>
-                <h5>{formatPrice(item.price)}</h5>
-              </div>
-            </div>
-          </div>
-        );
-      });
-
-      tabPanels.push(tabPanel);
-    });
-
+  const [pageNumber, setPageNumber] = useState(0);
+  const productsPerPage = 8;
+  const pagesVisited = pageNumber * productsPerPage;
+  const displayProducts = featProducts.all.products.slice(pagesVisited, pagesVisited + productsPerPage).map((item, index) => {
     return (
-      <Tabs defaultChecked={1}>
-        <TabList>{tabList}</TabList>
-        {tabPanels.map((item, index) => (
-          <TabPanel key={index}>
-            <div className="row">{item}</div>
-          </TabPanel>
-        ))}
-      </Tabs>
+      <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12" key={index}>
+      <div className="featured__item pl-pr-10">
+        <div className="featured__item__pic set-bg" style={{backgroundImage: `url(${item.img})`}}>
+          <ul className="featured__item__pic__hover">
+            <li >
+              <AiOutlineEye />
+            </li>
+            <li>
+              <AiOutlineShoppingCart />
+            </li>
+          </ul>
+        </div>
+        <div className="featured__item__text">
+          <h6>
+            <Link to="">{item.name}</Link>
+          </h6>
+          <h5>{formatPrice(item.price)}</h5>
+        </div>
+      </div>
+    </div>
     );
+  });
+  const pageCount = Math.ceil(featProducts.all.products.length / productsPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
   };
 
-  
   return (
     <>
       <div className="container">
@@ -862,11 +841,24 @@ const HomePage = () => {
           <div className="section-title">
             <h2>Khám phá tất cả các loại sản phẩm</h2>
           </div>
-          {renderFeaturedProducts(featProducts)}
+          {/* Hiển thị danh sách sản phẩm */}
+          <div className="row">{displayProducts}</div>
+
+          {/* Hiển thị phân trang */}
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"pagination"}
+            previousLinkClassName={"pagination__link"}
+            nextLinkClassName={"pagination__link"}
+            disabledClassName={"pagination__link--disabled"}
+            activeClassName={"pagination__link--active"}
+          />
         </div>
       </div>
     </>
   );
 };
-
 export default memo(HomePage);
