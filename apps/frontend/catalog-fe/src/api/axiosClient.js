e48ddf996ef3d9511fcd00dc6a3277b53f83800c
Hoàng Văn Hiếu <hieu.hoang2901@hcmut.edu.vn>
2024-01-08 01:39:08 +0700

    add env

--- apps/frontend/catalog-fe/src/api/axiosClient.js
+++ apps/frontend/catalog-fe/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL: process.env.REACT_APP_CATALOG_MS_1_URL,
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": '*',
