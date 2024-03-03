c7e088793854f6dca821fb05299ccd3a81368e2f
Hoang Anh <hoanganhleboy@gmail.com>
2024-03-03 13:43:07 +0700

    little change

--- deploy/k8s-ms/testing/api-stress-test-2.js
+++ deploy/k8s-ms/testing/api-stress-test-2.js
@@ -10,19 +10,19 @@ import http from "k6/http";
 // };
 
 export let options = {
-    stages: [
-        { duration: "1m", target: 50 },
-        { duration: '1m', target: 500 },
-        { duration: '2m', target: 1000 },
-        { duration: '2m', target: 1200 },
-        { duration: "2m", target: 10 },
-    ],
+  stages: [
+    { duration: "1m", target: 50 },
+    { duration: '1m', target: 500 },
+    { duration: '2m', target: 1000 },
+    { duration: '2m', target: 1200 },
+    { duration: "2m", target: 10 },
+  ],
 };
 
 export default function () {
-    let r = http.get(`http://192.168.49.2/catalog`);
-    check(r, {
-        "status is 200": (r) => r.status === 200,
-    });
-    sleep(3);
+  let r = http.get(`http://192.168.49.2/catalog`);
+  check(r, {
+    "status is 200": (r) => r.status === 200,
+  });
+  sleep(3);
 }
