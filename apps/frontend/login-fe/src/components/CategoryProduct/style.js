4dfcc6e8f7d483c67d2ffb82c6f8e80440c65447
Aquarius <hvh2901@gmail.com>
2024-03-10 15:02:58 +0700

    fix category

--- apps/frontend/login-fe/src/components/CategoryProduct/style.js
+++ apps/frontend/login-fe/src/components/CategoryProduct/style.js
@@ -4,7 +4,7 @@ export const WrapperCategoryItem = styled.div`
     align-items: center;
     gap: 24px;
     justify-content: flex-start;
-    border-bottom:1px solid black;
+    border-bottom:2px solid #008874;
     font-size: 24px;
     height: 44px;
     margin-top: 15px;
