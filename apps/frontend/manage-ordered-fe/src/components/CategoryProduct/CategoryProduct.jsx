5baa295e46c6051f964f18c4eb9da4cf98415023
Aquarius <hvh2901@gmail.com>
2024-03-18 22:35:01 +0700

    "update"

--- apps/frontend/manage-ordered-fe/src/components/CategoryProduct/CategoryProduct.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {WrapperCategoryItem} from './style'
-const CategoryProduct = ({categoryName}) => {
-  return (
-    <WrapperCategoryItem>{categoryName}</WrapperCategoryItem>
-  )
-}
-
-export default CategoryProduct
--- apps/frontend/shopping-cart-fe/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterSlice from './slice/counterSlice'
-export const store = configureStore({
-  reducer: {
-    counter:counterSlice
-  },
-})
