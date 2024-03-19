566fa0b4b764ab0ee4a89d5072d7d76653bd5264
Thới Duy Phát <88571858+duyphatbk@users.noreply.github.com>
2024-03-19 11:10:56 +0700

    Rearrange folders of services

---
+++ apps/backend/order-ms/src/main/java/com/service/order/OrderApplication.java
@@ -0,0 +1,26 @@
+package com.service.order;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.service.order.services.ReadData;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class OrderApplication {
+	@Autowired
+	public ReadData readData;
+	private static final Logger logger = LoggerFactory.getLogger(OrderApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+	@PostConstruct
+    public void init()  {
+		readData.readOrders("catalog.json");
+		readData.readUsers("user.json");
+		logger.info("Users Saved!");
+    }
+}
---
+++ apps/backend/order-ms/src/main/java/com/service/order/controllers/ProductController.java
@@ -0,0 +1,71 @@
+package com.service.order.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.service.order.models.Product;
+import com.service.order.models.ProductMessage;
+import com.service.order.rabbitmq.RabbitMQProducer;
+import com.service.order.reposistory.ProductRepository;
+import com.service.order.services.ProductResponseListener;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    
+    
+    @Autowired
+    ProductRepository productReposistory;
+
+    @Autowired
+    private RabbitMQProducer rabbitMQProducer;
+
+    @Autowired
+    public ProductMessage productMessage;
+
+    @Autowired
+    private ProductResponseListener productResponseListener;
+
+    @GetMapping("/product")
+    public List<Product> findAllProduct() {
+        productMessage.setAction("get");
+        productMessage.setInstance("product");
+
+        rabbitMQProducer.sendMessage(productMessage);
+        
+        List<Product> responseProducts = productResponseListener.waitForResponse();
+        
+        return responseProducts;
+    }
+    // @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    // public Product findProductById(@PathVariable Long productId){
+    // return productReposistory.findById(productId).orElse(null);
+    // return productReposistory.findById(productId).orElse(null);
+    // }
+    // @GetMapping("/productName/{productName}")
+    // public List<Product> findProductByName(@PathVariable String productName){
+    // return productReposistory.findByName(productName);
+    // return productReposistory.findByName(productName);
+    // }
+    // @GetMapping("/orderdById")
+    // public List<Product> orderProductById(){
+    // return productReposistory.OrderProductById();
+    // return productReposistory.OrderProductById();
+    // }
+    // @GetMapping("/IdAndInventory")
+    // public List<Product> findByCategory_IdAndInventory_Id(@RequestParam String
+    // category_id, @RequestParam String inventory_id){
+    // return productReposistory.findByCategory_IdAndInventory_Id(category_id,
+    // inventory_id);
+    // public List<Product> findByCategory_IdAndInventory_Id(@RequestParam String
+    // category_id, @RequestParam String inventory_id){
+    // return productReposistory.findByCategory_IdAndInventory_Id(category_id,
+    // inventory_id);
+    // }
+    // @GetMapping("/greaterThan/{price}")
+    // public List<Product> findByPriceGreaterThan(@PathVariable BigDecimal price){
+    // return productReposistory.findByPriceGreaterThan(price);
+    // return productReposistory.findByPriceGreaterThan(price);
+    // }
+}
